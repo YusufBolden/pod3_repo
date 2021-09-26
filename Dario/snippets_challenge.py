25493ed9326779486ec2f39089fc22fd181ed6c4
suenodivino <depena210@gmail.com>
2021-09-26 01:06:03 -0400

    Added completed snippets challenge to my repo

--- Dario/snippets_challenge.py
+++ Dario/snippets_challenge.py
@@ -8,7 +8,7 @@ print("Code Snippet 1:")
 u = 5
 v = 2
 
-if u * v = 10:
+if u * v == 10:
     print(f"The product of u ({u}) and v ({v}) is 10")
 else:
     print(f"The product of u ({u}) and v ({v}) is not 10")
@@ -18,16 +18,15 @@ print()
 print("Code Snippet 2:")
 x = 15
 y = 25
-
 z = 30
 
 if z < x:
     print("z is less than x")
 
-elif z > x and z < y
+elif z > x and z < y:
     print("z is between x and y")
 
-else
+else:
     print("z is greater than y")
 
 
@@ -38,7 +37,7 @@ print("Code Snippet 3:")
 #modify the comparison operator below so the assert statement passes
 #update the print statement to reflect changes to the code
 
-a = 1
+a = 2
 b = 1
 c = (a > b)
 
@@ -51,8 +50,9 @@ print("Code Snippet 4:")
 
 #modify exactly one boolean operator in the assignment of d, so that d evaluates to False
 
-d = (5 < 7) or not (8 < 20)
+d = (5 > 7) or not (8 < 20)
 # TO DO: Explain how d is set to False in a print statement
+print(f"The value of d ({d}) is False since 5 is not greater than 7, and 8 is less than 20.")
 assert(d == False) #Do not change this line
 
 print()
@@ -66,7 +66,7 @@ print("Code Snippet 5:")
 m = "GOAT"
 n = "goat"
 
-o = (m == n)
+o = (not m == n)
 
 print (f"The value of o ({o}) is True since Python is case-sensitive.")
 assert(o == True) #Do not change this line
