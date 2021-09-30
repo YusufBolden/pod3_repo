5e2aec0242a549cc402cfacf09dfd01d9a773d4f
suenodivino <depena210@gmail.com>
2021-09-29 22:34:27 -0400

    Added completed email ids challenge to my repo

--- Dario/stocks_challenge.py
+++ Dario/stocks_challenge.py
@@ -12,7 +12,13 @@ msft = 200
 
 print("Challenge 3.2.1: Taking user input")
 # TODO: Write code to ask the client his name and save it to a variable.
+name = input("Enter full name: ")
+print(name)
 # TODO: Write code to ask the client his savings and save it to another variable.
+savings = input("Enter savings: ")
+savings = int(savings)
+print(savings)
+print()
 # TODO: Write code to ask the client the stock he is interested in and save it to another variable, as shown below.
 stock = input("Which stock are you interested in? Type 'amzn' for Amazon, 'aapl' for Apple, 'fb' for Facebook, 'goog' for Google and 'msft' for Microsoft.")
 print()
@@ -28,13 +34,24 @@ if stock == "amzn":
 elif ...
 else ...
 '''
-
+if stock == "amzn":
+    print(savings/amazon)
+elif stock == "goog":
+    print(savings/google)
+elif stock == "fb":
+    print(savings/fb)
+elif stock == "msft":
+    print(savings/msft)
+elif stock == "aapl":
+    print(savings/apple)
+else:
+    print("insufficient funds")
 print()
 
 print("Challenge 3.2.3: Output for the user the result")
-# TODO: COnce you have calculated the number of stocks that can be purchased, print the result for the client. Result should be in a format like this:
+# TODO: Once you have calculated the number of stocks that can be purchased, print the result for the client. Result should be in a format like this:
 
 # Alex has $5000 in savings and he can buy 50 shares of Apple at the current price of $100.
-
-print()
+result = f"{name} has ${savings} in savings and can buy {savings/stock} shares of {stock} at the current price of $"
+print(result)
 
