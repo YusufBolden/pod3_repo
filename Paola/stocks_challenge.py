df3dc0f29aba3b3965e8637d837cc23f46072bc0
paolafern611 <paolafern611@gmail.com>
2021-10-11 17:09:14 -0400

    adding all my challenges sorry

--- Paola/stocks_challenge.py
+++ Paola/stocks_challenge.py
@@ -11,10 +11,12 @@ google = 1400
 msft = 200
 
 print("Challenge 3.2.1: Taking user input")
-# TODO: Write code to ask the client his name and save it to a variable.
-# TODO: Write code to ask the client his savings and save it to another variable.
+# TODO: Write code to ask the client his name and save it to a variable. 
+# TODO: Write code to ask the client his savings and save it to another variable. 
 # TODO: Write code to ask the client the stock he is interested in and save it to another variable, as shown below.
-stock = input("Which stock are you interested in? Type 'amzn' for Amazon, 'aapl' for Apple, 'fb' for Facebook, 'goog' for Google and 'msft' for Microsoft.")
+Name = input('What is your name? ')
+Savings = int(input('How much do you have in savings? '))
+Stock =input("Which stock are you interested in? Type 'amzn' for Amazon, 'aapl' for Apple, 'fb' for Facebook, 'goog' for Google and 'msft' for Microsoft.  ")
 print()
 
 print("Challenge 3.2.2: Perform user-specific calculations")
@@ -28,6 +30,24 @@ if stock == "amzn":
 elif ...
 else ...
 '''
+if Stock == 'amzn': 
+    amzn = Shares=(Savings/amazon)
+    print(f'{Name} has ${Savings} in Savings and he can buy {Shares} shares of Amazon at the current price of 3000')
+elif Stock == 'aapl':
+    aapl = Shares=(Savings/apple)
+    print(f'{Name} has ${Savings} in Savings and he can buy {Shares} shares of Apple at the current price of 100')
+elif Stock == 'fb':
+    fb = Shares=(Savings/facebook)
+    print(f'{Name} has ${Savings} in Savings and he can buy {Shares} shares of Facebook at the current price of 250')
+elif Stock == 'goog':
+    goog = Shares= (Savings/google)
+    print(f'{Name} has ${Savings} in Savings and he can buy {Shares} shares of Google at the current price of 1400')
+elif Stock == 'msft':
+    msft = Shares=(Savings/microsoft)
+    print(f'{Name} has ${Savings} in Savings and he can buy {Shares} shares of Microsoft at the current price of 200')
+else:
+    print('Invalid Name, Please try again')
+
 
 print()
 
