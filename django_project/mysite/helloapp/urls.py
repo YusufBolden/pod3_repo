cdfed48963256d8c2dcba77ae117de3fccbbc8d5
YusufBolden <ajb2316@columbia.edu>
2021-10-26 20:18:38 -0400

    Delete duplicate django project folder

--- django_project/mysite/helloapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from helloapp.views import hello, goodbye
-
-urlpatterns = [
-    # helloapp/
-    path('', hello, name='hello'),
-    path('<str:name>', hello, name='hello_name'),
-    path('goodbye/django', goodbye, name='goodbye_name')
-]
--- django_project/mysite/helloapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def hello(request, name='World!'):
-    if request.method == 'GET':
-        return render(request, 'hello.html', context = { 'name': name})
-
-def goodbye(request, name='Django!'):
-    if request.method == 'GET':
-        return render(request, 'goodbye.html', context = { 'name': name})
