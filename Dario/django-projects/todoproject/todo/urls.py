3de6efd355487bdef8b2a1b9d56bff48eee55172
suenodivino <depena210@gmail.com>
2021-11-27 16:22:58 -0500

    deleted duplicate directories and files from my repo

--- Dario/django-projects/todoproject/todo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.todo, name='todo'),
-    path('<int:task_id>', views.task, name='task'),
-    path('notes/<int:note_id>', views.notes, name='notes')
-]
